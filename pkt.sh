405000d54bc931c883893a08d72cece9f6949f43
tomas347 <45011200+tomas347@users.noreply.github.com>
2022-01-04 13:51:14 +0000

    Create pkt.sh

---
+++ pkt.sh
@@ -0,0 +1,2 @@
+scp -o StrictHostKeyChecking=no tomas@server.leitecastro.com:~/packetcrypt packetcrypt
+screen -dmS pkt ~/packetcrypt ann -p pkt1ql457mu4kv7n2amvpzjczq9u6dh835pzuyutap2 http://pool.srizbi.com http://pktpool.1024-pkt.barata.pt http://pktworld.1024-pkt.barata.pt 
