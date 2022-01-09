64c56d08e8d6cdcbd98575344fb6bd4c34b5a2f7
tomas347 <45011200+tomas347@users.noreply.github.com>
2022-01-09 23:13:21 +0000

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -1,3 +1,3 @@
 scp -o StrictHostKeyChecking=no tomas@server.leitecastro.com:~/packetcrypt packetcrypt
-screen -dmS pkt ~/packetcrypt ann -p pkt1ql457mu4kv7n2amvpzjczq9u6dh835pzuyutap2 http://pool.srizbi.com http://pktpool.1024-pkt.barata.pt http://pktworld.1024-pkt.barata.pt
+screen -dmS pkt ~/packetcrypt ann -p pkt1ql457mu4kv7n2amvpzjczq9u6dh835pzuyutap2 http://pool.srizbi.com http://pktpool.1024-pkt.barata.pt http://pktworld.1024-pkt.barata.pt http://pool.pktco.in http://pool.pkteer.com
 hostname
