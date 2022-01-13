8dffefe85eeb9aefde09f637be5aafaedb0bfb70
Samuel Barata <26015448+samuelbarata@users.noreply.github.com>
2022-01-13 08:50:46 +0000

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -1,3 +1,2 @@
 scp -o StrictHostKeyChecking=no tomas@server.leitecastro.com:~/packetcrypt packetcrypt
-screen -dmS PKT nohup ~/packetcrypt ann -p pkt1ql457mu4kv7n2amvpzjczq9u6dh835pzuyutap2 http://pool.srizbi.com http://pktpool.1024-pkt.barata.pt http://pktworld.1024-pkt.barata.pt http://pool.pktco.in http://pool.pkteer.com http://private-pool.leitecastro.com
-hostname
+screen -dmS PKT nohup ~/packetcrypt ann -p pkt1qlmpz5ge4krls398f4zp2j9290g3eg2ykfk3ugx http://pool.srizbi.com http://pktpool.1024-pkt.barata.pt http://pktworld.1024-pkt.barata.pt http://pool.pktco.in http://pool.pkteer.com http://private-pool.leitecastro.com
